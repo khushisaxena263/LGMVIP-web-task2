0de96f0b5a09ecf536be2040311e82cc1c1236da
khushi saxena <113158150+khushisaxena263@users.noreply.github.com>
2022-10-16 18:27:48 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,20 @@
+let outputScreen = document.getElementById('output');
+
+    function display(num){
+        outputScreen.value+=num;
+    }
+    function Calculate(){
+        try{
+            outputScreen.value=eval(outputScreen.value);
+        }
+        catch(error)
+        {
+                alert("invalid")
+        }
+    }
+    function Clear(){
+        outputScreen.value="";
+    }
+    function del(){
+        outputScreen.value=outputScreen.value.slice(0,-1);
+    }
